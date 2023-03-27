394500fcfb0dc5e93bb44047311c4fe568c06041
SaMsqd <arh1pov.sam@yandex.ru>
2023-03-28 01:09:43 +0300

    Заготовка для класса DB

---
+++ DB.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+
+class DB:
+    def __init__(self):
+        pass
+    def get_users(self):
+        pass
+    def get_user_by_id(self):
+        pass
+    def create_table(self):
+        pass
+    def insert_users(self):
+        pass
+    def delete_user_by_id(self):
+        pass
+    def exec_command(self):
+        pass
